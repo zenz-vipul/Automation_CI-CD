49fa350786f91f7fd8ab55066fbe78309bdbc26d
Vipul Jujar <vipuljujar@Sarva.local>
2024-10-04 14:51:47 +0530

    iome updated code

--- iome/iome_web.py
+++ iome/iome_web.py
@@ -1,66 +1,41 @@
-import os
 import time
-import pytest
 from selenium import webdriver
+import pytest
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.chrome.options import Options
 
-chrome_options = Options()
-chrome_options.add_argument("--headless") 
+class Test_landing_page:
+    def setup_method(self):
+        chrome_options = Options()
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+        self.driver = webdriver.Chrome(options=chrome_options)
 
-@pytest.fixture(scope="module")
-def driver():
-    driver = webdriver.Chrome()  
-    url = os.environ.get('URL') or 'https://iome.ai'
-    driver.get(url)
-    yield driver
-    driver.quit()
+    def teardown_method(self):
+        self.driver.quit()
 
-def test_nav_links(driver):
-    try:
-        print("Waiting for page to be fully loaded...")
-        WebDriverWait(driver, 60).until(
-            EC.presence_of_element_located((By.TAG_NAME, 'body'))
-        )
-        print("Page is fully loaded!")
-        
-        print("Waiting for navbar to be visible...")
-        navbar = WebDriverWait(driver, 60).until(
-            EC.visibility_of_element_located((By.CSS_SELECTOR, '.ant-col.flex.justify-end'))
-        )
-        print("Navbar is visible!")
-        nav_links = navbar.find_elements(By.TAG_NAME, 'a')
+    def test_digital_you(self):
+        self.driver.get("https://iome.ai")
+        self.driver.find_element(By.XPATH, '//a[@href="/#the-digital-you"]').click()
+        time.sleep(2)
+        self.driver.back()
 
-        for link in nav_links:
-            link_name = link.text.strip()
-            link_url = link.get_attribute("href")
+    def test_developer(self):
+        self.driver.get("https://iome.ai")
+        self.driver.find_element(By.XPATH, '//a[@href="https://dev.iome.ai"]').click()
+        time.sleep(2)
+        self.driver.back()
 
-            if not link_name or "mailto:" in link_url:
-                print(f"Skipping link: {link_name or 'empty'}")
-                continue
-            
-            print(f"Testing navbar link: {link_name} -> {link_url}")
-            driver.get(link_url)
+    def test_community(self):
+        self.driver.get("https://iome.ai")
+        self.driver.find_element(By.XPATH, '//a[@href="https://join.slack.com/t/iomeai/shared_invite/zt-20s1w9jxg-unzBomKqMBrrq~DlYNpQHQ"]').click()
+        time.sleep(2)
+        self.driver.back()
 
-            expected_url = {
-                "Digital You": "https://iome.ai/#the-digital-you",
-                "Developer": "https://dev.iome.ai/",
-            }
-            
-            if link_name in expected_url:
-                assert driver.current_url == expected_url[link_name], f"Navigation Link '{link_name}' did not redirect correctly or is broken."
-            else:
-                print(f"Skipping validation for link: {link_name}")
-
-            driver.back()
-            WebDriverWait(driver, 10).until(
-                EC.visibility_of_element_located((By.CSS_SELECTOR, '.ant-col.flex.justify-end'))
-            )  
-
-    except TimeoutException:
-        pytest.fail("Timeout: Navbar could not be located.")
-    except Exception as e:
-        pytest.fail(f"Error locating links: {e}")
+    def test_go_to_app(self):
+        self.driver.get("https://iome.ai")
+        go_to_app_link = self.driver.find_element(By.XPATH, '//button//span[contains(text(), "Go to app")]/..')
+        go_to_app_link.click()
+        time.sleep(2) 
+        self.driver.back()
