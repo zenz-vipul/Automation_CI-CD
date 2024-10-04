86391406911a29e513315d15e170b42e31c65c80
Vipul Jujar <vipuljujar@Sarva.local>
2024-10-04 11:46:17 +0530

    iome_web updated code

--- iome/iome_web.py
+++ iome/iome_web.py
@@ -21,45 +21,51 @@ def driver():
 
 def test_digital_you_link(driver):
     driver.get(PAGE_URL)
-    digital_you = WebDriverWait(driver, 10).until(
+    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'a[href="/#the-digital-you"]')))
+    digital_you = WebDriverWait(driver, 20).until(
         EC.element_to_be_clickable((By.CSS_SELECTOR, 'a[href="/#the-digital-you"]'))
     )
     digital_you.click()
-    WebDriverWait(driver, 10).until(EC.url_contains('/#the-digital-you'))
+    WebDriverWait(driver, 20).until(EC.url_contains('/#the-digital-you'))
     assert driver.current_url.endswith('/#the-digital-you')
 
 def test_developer_link(driver):
     driver.get(PAGE_URL)
+    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'a[href="https://dev.iome.ai"]')))
     
-    developer_link = WebDriverWait(driver, 10).until(
+    developer_link = WebDriverWait(driver, 20).until(
         EC.element_to_be_clickable((By.CSS_SELECTOR, 'a[href="https://dev.iome.ai"]'))
     )
     assert developer_link.get_attribute('target') == '_blank'
     developer_link.click()
     driver.switch_to.window(driver.window_handles[-1])
-    WebDriverWait(driver, 10).until(EC.url_contains('dev.iome.ai'))
+    WebDriverWait(driver, 20).until(EC.url_contains('dev.iome.ai'))
     assert 'dev.iome.ai' in driver.current_url
     driver.close()
     driver.switch_to.window(driver.window_handles[0])
 
 def test_community_link(driver):
     driver.get(PAGE_URL)
-    community_link = WebDriverWait(driver, 10).until(
+    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'a[href="https://join.slack.com/t/iomeai/shared_invite/zt-20s1w9jxg-unzBomKqMBrrq~DlYNpQHQ"]')))
+    
+    community_link = WebDriverWait(driver, 20).until(
         EC.element_to_be_clickable((By.CSS_SELECTOR, 'a[href="https://join.slack.com/t/iomeai/shared_invite/zt-20s1w9jxg-unzBomKqMBrrq~DlYNpQHQ"]'))
     )
     assert community_link.get_attribute('target') == '_blank'
     community_link.click()
     driver.switch_to.window(driver.window_handles[-1])
-    WebDriverWait(driver, 10).until(EC.url_contains('slack.com'))
+    WebDriverWait(driver, 20).until(EC.url_contains('slack.com'))
     assert 'slack.com' in driver.current_url
     driver.close()
     driver.switch_to.window(driver.window_handles[0])
 
 def test_go_to_app_button(driver):
     driver.get(PAGE_URL)
-    go_to_app_button = WebDriverWait(driver, 10).until(
+    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'a[href="/login/"] button')))
+    
+    go_to_app_button = WebDriverWait(driver, 20).until(
         EC.element_to_be_clickable((By.CSS_SELECTOR, 'a[href="/login/"] button'))
     )
     go_to_app_button.click()
-    WebDriverWait(driver, 10).until(EC.url_contains('/login/'))
+    WebDriverWait(driver, 20).until(EC.url_contains('/login/'))
     assert driver.current_url.endswith('/login/')
